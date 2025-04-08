3556c56169428ff84330380eaa67749ff4ca67ba
crazywoola <427733928@qq.com>
2025-04-08 16:36:20 +0800

    feat: init formula

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # homebrew-dify
+
+Homebrew tag for Dify
+## Installation
+
+```bash
+brew tap crazywoola/dify
+brew install dify
+```
---
+++ dify.rb
@@ -0,0 +1,27 @@
+class Dify < Formula
+    desc "Dify"
+    homepage "https://github.com/langgenius/dify-plugin-daemon"
+    license "MIT"
+    url "https://github.com/langgenius/dify-plugin-daemon/archive/refs/tags/0.0.6.tar.gz"
+    sha256 "d7109828c5d0e162387a744b33cefbb908f943e5c30e1d95ddd900733ed319d9"
+
+    livecheck do
+        url :stable
+        strategy :github_latest
+    end
+
+    bottle do
+        sha256 cellar: :any_skip_relocation, all: "d7109828c5d0e162387a744b33cefbb908f943e5c30e1d95ddd900733ed319d9"
+    end
+
+    depends_on "go" => :build
+
+    def install
+       ldflags = "-X 'main.VersionX=v#{version}'"
+       system "go", "build", *std_go_args(ldflags: ldflags), "-o", bin/"dify", "./cmd/commandline"
+    end
+
+    test do
+        assert_match version.to_s, shell_output("#{bin}/dify version")
+    end
+end

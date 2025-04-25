6dd995b2b9ac1fd09862e8f7730ab5705073928e
Bowen Liang <liangbowen@gf.com.cn>
2025-04-26 01:48:08 +0800

    update

--- dify.rb
+++ dify.rb
@@ -16,33 +16,27 @@ class Dify < Formula
     "dify-plugin-#{os_name}-#{arch_name}"
   end
 
-  SHA256_FILE = "cli_checksums.txt"
-  def self.sha256_map
-    @sha256_map ||= begin
-                      map = {}
-                      File.readlines(SHA256_FILE).each do |line|
-                        name, sha = line.strip.split
-                        map[name] = sha
-                      end
-                      map
-                    rescue Errno::ENOENT
-                      raise "Failed to read the SHA256 checksum list file #{SHA256_FILE}"
-                    end
-  end
+  def self.cli_sha256
+    sha256sum_map = {}
+    File.readlines("cli_sha256sum_list.txt").each do |line|
+      name, sha = line.strip.split
+      sha256sum_map[name] = sha
+    end
 
-  def self.dynamic_sha256
-    sha256_map.fetch(cli_bin_name) do |key|
-      raise "Failed to look up SHA256 checksum for #{key}"
+    sha256sum_map.fetch(cli_bin_name) do |key|
+      raise "Failed to look up SHA256 checksum for `#{key}`"
     end
   end
 
   # Define the URL and the SHA256 checksum for binary file
   url "#{homepage}/releases/download/#{version}/#{cli_bin_name}"
-  sha256 dynamic_sha256
+  sha256 "#{cli_sha256}"
 
   def install
     # move the binary file to bin directory
     bin.install "#{self.class.cli_bin_name}" => "dify"
+    system "chmod +x #{bin}/dify"
+    system "#{bin}/dify version"
   end
 
   test do

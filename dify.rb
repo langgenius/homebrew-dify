c9d90707220ea1127362da9868406b1117957eb7
Bowen Liang <liangbowen@gf.com.cn>
2025-04-26 02:07:48 +0800

    simplify

--- dify.rb
+++ dify.rb
@@ -9,37 +9,28 @@ class Dify < Formula
     strategy :github_latest
   end
 
-  def self.os_name
-    OS.mac? ? "darwin" : "linux"
-  end
+  os_name = OS.mac? ? "darwin" : (OS.linux? ? "linux" : "unknown")
+  arch_name = Hardware::CPU.arm? ? "arm64" : (Hardware::CPU.intel? ? "amd64" : "unknown")
+  CLI_BIN_NAME = "dify-plugin-#{os_name}-#{arch_name}"
 
-  def self.arch_name
-    Hardware::CPU.arm? ? "arm64" : "amd64"
-  end
-
-  def self.cli_bin_name
-    "dify-plugin-#{os_name}-#{arch_name}"
-  end
-
-  def self.cli_sha256
+  def self.cli_sha256(cli_name)
     sha256sum_map = {}
     File.readlines("cli_sha256sum_list.txt").each do |line|
       name, sha = line.strip.split
       sha256sum_map[name] = sha
     end
-
-    sha256sum_map.fetch(cli_bin_name) do |key|
+    sha256sum_map.fetch(cli_name) do |key|
       raise "Failed to look up SHA256 checksum for `#{key}`"
     end
   end
 
   # Define the URL and the SHA256 checksum for binary file
-  url "#{homepage}/releases/download/#{version}/#{cli_bin_name}"
-  sha256 "#{cli_sha256}"
+  url "#{homepage}/releases/download/#{version}/#{CLI_BIN_NAME}"
+  sha256 cli_sha256(CLI_BIN_NAME)
 
   def install
     # move the binary file to bin directory
-    bin.install "#{self.class.cli_bin_name}" => "dify"
+    bin.install "#{CLI_BIN_NAME}" => "dify"
     system "chmod +x #{bin}/dify"
     system "#{bin}/dify version"
   end
